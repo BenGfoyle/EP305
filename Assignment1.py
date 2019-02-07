0837943eaa0b8b9ea63cc7c89ee34de1a455eef4
BenGfoyle <40490843+BenGfoyle@users.noreply.github.com>
2019-02-07 10:55:31 +0000

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,3 +1,3 @@
 n1 = int(input("Please enter an integer")) #Takes in input, parses to integer
 n2 = int(input("Please enter another integer"))
-print(n1,  "Squared =", n1**2, "and", n2, "Squared =2", n2**2) #Print/square numbers
+print(n1,  "Squared =", n1**2, "and", n2, "Squared =", n2**2) #Print/square numbers
